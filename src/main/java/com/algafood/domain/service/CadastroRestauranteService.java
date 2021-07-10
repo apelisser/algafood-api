1869516d42d627e6a65b12c2abc2e301eda2c10b
Abner J Pelisser <abner.pelisser@gmail.com>
2021-07-10 15:07:15 -0300

    Implementando a inclusão de restaurantes e tratando cozinhas inexistentes - vídeo 4.30

--- src/main/java/com/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algafood/domain/service/CadastroRestauranteService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.algafood.domain.exception.EntidadeEmUsoException;
 import com.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.model.Restaurante;
+import com.algafood.domain.repository.CozinhaRepository;
 import com.algafood.domain.repository.RestauranteRepository;
 
 @Service
@@ -16,7 +18,20 @@ public class CadastroRestauranteService {
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
 	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+		
+		restaurante.setCozinha(cozinha);
+		
 		return restauranteRepository.salvar(restaurante);
 	}
 
