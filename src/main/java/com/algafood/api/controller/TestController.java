08ea1b6cf6cac8232a7a12a8b968bc54b3762fdd
Abner J Pelisser <abner.pelisser@gmail.com>
2021-07-27 22:24:23 -0300

    Implementação de consultas personalizadas utilizando SpringDataJPA - vídeo 5.7

--- src/main/java/com/algafood/api/controller/TestController.java
+++ src/main/java/com/algafood/api/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.algafood.api.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.domain.model.Cozinha;
+import com.algafood.domain.model.Restaurante;
 import com.algafood.domain.repository.CozinhaRepository;
+import com.algafood.domain.repository.RestauranteRepository;
 
 @RestController
 @RequestMapping("/teste")
@@ -18,9 +21,26 @@ public class TestController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//	}
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.findByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/por-taxa-frete")
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			@RequestParam("taxaInicial") BigDecimal taxaInicial, 
+			@RequestParam("taxaFinal") BigDecimal taxaFinal ) {
+		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+	}
+	
+	@GetMapping("/restaurantes/por-nome")
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			@RequestParam("nome") String nome, 
+			@RequestParam("cozinhaId") Long cozinhaId) {
+		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+	}
 	
 }
