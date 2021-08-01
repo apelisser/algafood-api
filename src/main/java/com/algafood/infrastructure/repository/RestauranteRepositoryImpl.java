df7057ab644cd111f8c995d5e862f947950450b8
Abner J Pelisser <abner.pelisser@gmail.com>
2021-08-01 16:43:02 -0300

    Inserido os predicates (where) na consulta com Criteria (não está dinâmico ainda) - vídeo 5.14

--- src/main/java/com/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -25,8 +27,14 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 
-		criteria.from(Restaurante.class); // from restaurante
+		Root<Restaurante> root = criteria.from(Restaurante.class); // from restaurante
 
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+		Predicate taxaInicialPredicaete = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicaete = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		
+		criteria.where(nomePredicate, taxaInicialPredicaete, taxaFinalPredicaete);
+		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
