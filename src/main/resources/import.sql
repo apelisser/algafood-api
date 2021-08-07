7dbb1abc8ef80d7ab21e04572f7e521259cbcb79
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-08-07 15:55:01 -0300

    Criando o mapeamento de muitos para muitos entre restaurantes e formas de pagamento - vídeo 6.2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,4 +5,10 @@ insert into restaurante(nome, taxa_frete, cozinha_id) values ('Restaurante da ba
 insert into restaurante(nome, taxa_frete, cozinha_id) values ('Restaurante do pico', 0.7, 2);
 insert into restaurante(nome, taxa_frete, cozinha_id) values ('Restaurante da amizade', 10.7, 1);
 
+insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
+
 insert into estado(nome) values ('Paraná');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
