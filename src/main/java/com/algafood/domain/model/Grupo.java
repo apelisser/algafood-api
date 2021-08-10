75b783a5b910fde5f84a3bf1bdf138bf193d4623
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-08-10 02:26:50 -0300

    Criação das entidades Grupo e Usuario, e seus mapeamentos (muitos para muitos) - vídeo 6.9

---
+++ src/main/java/com/algafood/domain/model/Grupo.java
@@ -0,0 +1,60 @@
+package com.algafood.domain.model;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Grupo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao"
+	, joinColumns = @JoinColumn(name = "grupo_id")
+	, inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissoes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Permissao> getPermissoes() {
+		return permissoes;
+	}
+
+	public void setPermissoes(List<Permissao> permissoes) {
+		this.permissoes = permissoes;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Grupo other = (Grupo) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
