5e59eb15748f84b9c36480298d1f0b7f474f076d
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-08-29 01:00:02 -0300

    Removido o uso do Lombok

--- src/main/java/com/algafood/api/exceptionhandler/Problema.java
+++ src/main/java/com/algafood/api/exceptionhandler/Problema.java
@@ -2,14 +2,22 @@ package com.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
 public class Problema {
 	
 	private LocalDateTime dataHora;
 	private String mensagem;
 	
+	public Problema(String mensagem, LocalDateTime dataHora) {
+		this.mensagem = mensagem;
+		this.dataHora = dataHora;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
 }
