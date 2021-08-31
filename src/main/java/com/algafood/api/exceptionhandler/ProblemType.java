915e234358624c53eea1c82f86c93ee63ae3b67d
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-08-31 00:32:51 -0300

    Personalizado exceção para quando o corpo da requisição for inválido.

--- src/main/java/com/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algafood/api/exceptionhandler/ProblemType.java
@@ -2,6 +2,7 @@ package com.algafood.api.exceptionhandler;
 
 public enum ProblemType {
 
+	MENSAGEM_IMCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
