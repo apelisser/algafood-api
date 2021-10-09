1a96ecd8695be4e7505ba72252e436d5aa0a7c66
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-10-09 11:46:55 -0300

    Desafio de teste para exclusão de cozinha - vídeo 10.5

--- src/test/java/com/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algafood/CadastroCozinhaIntegrationTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algafood.domain.exception.EntidadeEmUsoException;
 import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.service.CadastroCozinhaService;
 
@@ -39,13 +41,33 @@ class CadastroCozinhaIntegrationTests {
 		novaCozinha.setNome(null);
 
 		// ação
-		ConstraintViolationException erroEsperado = 
+		ConstraintViolationException exception = 
 				Assertions.assertThrows(ConstraintViolationException.class, () -> {					
 					cadastroCozinha.salvar(novaCozinha);
 				});
 		
-		assertThat(erroEsperado).isNotNull();
+		assertThat(exception).isNotNull();
 		
 	}
+	
+	@Test
+    public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		EntidadeEmUsoException exception = 
+				Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
+					cadastroCozinha.excluir(1L);
+				});
+		
+		assertThat(exception).isNotNull();
+    }
+	
+	@Test
+    public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		CozinhaNaoEncontradaException exception = 
+				Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
+					cadastroCozinha.excluir(100L);
+				});
+		
+		assertThat(exception).isNotNull();
+    }
 
 }
