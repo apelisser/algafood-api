a9272dbb611f4cfcc877a863320894426967990c
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-10-09 12:23:19 -0300

    Iniciando os testes de API - get em cozinhas - vídeo 10.8

--- src/test/java/com/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algafood/CadastroCozinhaIT.java
@@ -8,18 +8,46 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import com.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algafood.domain.exception.EntidadeEmUsoException;
 import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.service.CadastroCozinhaService;
 
-@SpringBootTest
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIT {
 
+	@LocalServerPort
+	private int port;
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+	
+	
+	
+	// =================================================================================================
+	// ========================== TESTES DE INTEGRAÇÃO =================================================
+	// =================================================================================================
+	
+/*	
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
-
+	
 	@Test
 	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
 		// cenário
@@ -69,5 +97,6 @@ class CadastroCozinhaIT {
 		
 		assertThat(exception).isNotNull();
     }
+*/
 
 }
