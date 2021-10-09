5bdd7813f27959da33175c016f57deef42acb0c6
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-10-09 11:29:49 -0300

    Primeiro teste de integração criado, para cozinha - vídeo 10.3

---
+++ src/test/java/com/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,51 @@
+package com.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.algafood.domain.model.Cozinha;
+import com.algafood.domain.service.CadastroCozinhaService;
+
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+
+	@Test
+	public void testarCadastroCozinhaComSucesso() {
+		// cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+
+		// ação
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+		// validação
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+
+	@Test
+	public void testarCadastrarCozinhaSemNome() {
+		// cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+
+		// ação
+		ConstraintViolationException erroEsperado = 
+				Assertions.assertThrows(ConstraintViolationException.class, () -> {					
+					cadastroCozinha.salvar(novaCozinha);
+				});
+		
+		assertThat(erroEsperado).isNotNull();
+		
+	}
+
+}
