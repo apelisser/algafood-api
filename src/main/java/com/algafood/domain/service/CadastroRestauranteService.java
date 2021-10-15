82b6c689113caf2bfe264a056aad69affa18a304
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-10-15 01:22:15 -0300

    Métodos de manipulação de banco de dados ,das classes services, anotados para abrir transação - vídeo 111.1

--- src/main/java/com/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algafood/domain/service/CadastroRestauranteService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algafood.domain.exception.EntidadeEmUsoException;
 import com.algafood.domain.exception.RestauranteNaoEncontradoException;
@@ -22,6 +23,7 @@ public class CadastroRestauranteService {
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
 
+	@Transactional
 	public Restaurante salvar(Restaurante restaurante) {
 		Long cozinhaId = restaurante.getCozinha().getId();
 		Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
@@ -30,6 +32,7 @@ public class CadastroRestauranteService {
 		return restauranteRepository.save(restaurante);
 	}
 
+	@Transactional
 	public void excluir(Long restauranteId) {
 		try {
 			restauranteRepository.deleteById(restauranteId);
