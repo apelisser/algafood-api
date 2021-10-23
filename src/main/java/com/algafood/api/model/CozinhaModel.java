e846160bf92d585aae727be4cc827426e0e9d368
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-10-23 16:19:36 -0300

    Incluir arquivos que não estavam sendo assistidos pelo GIT

--- src/main/java/com/algafood/api/model/CozinhaModel.java
+++ src/main/java/com/algafood/api/model/CozinhaModel.java
@@ -1,21 +1,13 @@
 package com.algafood.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
 public class CozinhaModel {
 
 	private Long id;
 	private String nome;
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 }
