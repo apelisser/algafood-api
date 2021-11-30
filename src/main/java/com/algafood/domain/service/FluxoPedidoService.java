acbcacff3d0d3f61badb8356d1577f1c681474f1
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-11-30 18:34:04 -0300

    Desafio finalizado: implementado cancelamento e entrega de pedido - vídeo 12.23

--- src/main/java/com/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algafood/domain/service/FluxoPedidoService.java
@@ -24,10 +24,38 @@ public class FluxoPedidoService {
 		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
 			throw new NegocioException(
 					String.format("Status do pedido %d não pode ser alterado de %s para %s", 
-							pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CONFIRMADO));
+							pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CONFIRMADO.getDescricao()));
 		}
 		
 		pedido.setStatus(StatusPedido.CONFIRMADO);
+		pedido.setDataConfirmacao(OffsetDateTime.now());
+	}
+	
+	@Transactional
+	public void cancelar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+			throw new NegocioException(
+					String.format("Status do pedido %d não pode ser alterado de %s para %s", 
+							pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CANCELADO.getDescricao()));
+		}
+		
+		pedido.setStatus(StatusPedido.CANCELADO);
+		pedido.setDataConfirmacao(OffsetDateTime.now());
+	}
+	
+	@Transactional
+	public void entregar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		if (!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
+			throw new NegocioException(
+					String.format("Status do pedido %d não pode ser alterado de %s para %s", 
+							pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.ENTREGUE.getDescricao()));
+		}
+		
+		pedido.setStatus(StatusPedido.ENTREGUE);
 		pedido.setDataConfirmacao(OffsetDateTime.now());
 	}
 	
