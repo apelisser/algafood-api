99c7c5545c0cd9d13d5c47286967aae08d5d6d1c
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-12-14 21:57:22 -0300

    Criado recurso de estatística de vendas diárias - vídeo 13.14

--- src/main/java/com/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,16 +1,43 @@
 package com.algafood.infrastructure.service;
 
+
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.algafood.domain.filter.VendaDiariaFilter;
+import com.algafood.domain.model.Pedido;
 import com.algafood.domain.model.dto.VendaDiaria;
 import com.algafood.domain.service.VendaQueryService;
 
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService{
 
+	@Autowired
+	private EntityManager manager;
+	
 	@Override
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
-		return null;
+		var builder = manager.getCriteriaBuilder();
+		var query = builder.createQuery(VendaDiaria.class);
+		var root = query.from(Pedido.class);
+		
+		var funcionDateDataCriacao = builder.function(
+				"date", Date.class, root.get("dataCriacao"));
+		
+		var selection = builder.construct(VendaDiaria.class, 
+				funcionDateDataCriacao, 
+				builder.count(root.get("id")),
+				builder.sum(root.get("valorTotal")));
+		
+		query.select(selection);
+		query.groupBy(funcionDateDataCriacao);
+		
+		return manager.createQuery(query).getResultList();		
 	}
 	
 }
