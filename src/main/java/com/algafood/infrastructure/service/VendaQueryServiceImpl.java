2e185b7dc3c576a14f7e2bc3c29b798c29943451
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-12-14 22:15:48 -0300

    Adicionando filtros no recurso de  vendas diárias, com criteria api - vídeo 13.15

--- src/main/java/com/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,16 +1,19 @@
 package com.algafood.infrastructure.service;
 
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.algafood.domain.filter.VendaDiariaFilter;
 import com.algafood.domain.model.Pedido;
+import com.algafood.domain.model.StatusPedido;
 import com.algafood.domain.model.dto.VendaDiaria;
 import com.algafood.domain.service.VendaQueryService;
 
@@ -22,6 +25,7 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 	
 	@Override
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+		List<Predicate> predicates = new ArrayList<>();
 		var builder = manager.getCriteriaBuilder();
 		var query = builder.createQuery(VendaDiaria.class);
 		var root = query.from(Pedido.class);
@@ -33,6 +37,23 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 				funcionDateDataCriacao, 
 				builder.count(root.get("id")),
 				builder.sum(root.get("valorTotal")));
+		
+		if (filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(
+					root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		if (filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(
+					root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+		}
+		
+		if (filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+		}
+		
+		predicates.add(root.get("status")
+				.in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
 		
 		query.select(selection);
 		query.groupBy(funcionDateDataCriacao);
