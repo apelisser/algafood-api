1e9cac656f91cad79a0d2026a1fac2b6e7714cb9
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-12-21 08:50:48 -0300

    Implementado endpoint para receber upload de arquivo(imagem) - vídeo 14.2

---
+++ src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,37 @@
+package com.algafood.api.controller;
+
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algafood.api.model.input.FotoProdutoInput;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+	@PutMapping
+	public void atualizarFoto(@PathVariable Long restauranteId,
+			@PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+		
+		var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+		
+		var arquivoFoto = Path.of("/home/apelisser/Imagens/algafood/", nomeArquivo);
+		
+		System.out.println(arquivoFoto);
+		System.out.println(fotoProdutoInput.getArquivo().getContentType());
+		System.out.println(fotoProdutoInput.getDescricao());
+		
+		try {
+			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+}
