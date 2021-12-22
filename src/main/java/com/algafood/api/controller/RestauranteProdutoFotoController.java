783c0a83e33c8cbaf05111971a24173dee87e3f1
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-12-22 01:09:07 -0300

    Implementado um limite de tamanho, em bytes, da imagem enviada para a API - vídeo 14.3

--- src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -3,6 +3,8 @@ package com.algafood.api.controller;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,7 @@ public class RestauranteProdutoFotoController {
 
 	@PutMapping
 	public void atualizarFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
 		
 		var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 		
