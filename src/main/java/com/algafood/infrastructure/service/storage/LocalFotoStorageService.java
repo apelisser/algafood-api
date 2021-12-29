adead2fe3cdf1bb05069af9e62c1459ea432a455
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2021-12-28 21:28:46 -0300

    Desafio resolvido: recuperar arquivo - vídeo 14.11

--- src/main/java/com/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -1,6 +1,7 @@
 package com.algafood.infrastructure.service.storage;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -40,6 +41,17 @@ public class LocalFotoStorageService implements FotoStorageService {
 		}
 	}
 
+	@Override
+	public InputStream recuperar(String nomeArquivo) {
+		try {
+			Path arquivoPath = getArquivoPath(nomeArquivo);
+		
+			return Files.newInputStream(arquivoPath);
+		} catch (IOException e) {
+			throw new StorageException("Não foi possível recuperar arquivo.", e);
+		}	
+	}
+	
 	private Path getArquivoPath(String nomeArquivo) {
 		return diretorioFotos.resolve(Path.of(nomeArquivo));
 	}
