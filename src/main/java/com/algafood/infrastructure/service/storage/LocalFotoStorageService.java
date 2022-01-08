6bbef6e781f60039274ea86d139cb38985c22ad2
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2022-01-08 12:21:09 -0300

    Implementada uma classe de configuracao para o armazenamento de fotos (local e aws-s3) - vídeo 14.20

--- src/main/java/com/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -5,17 +5,18 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import com.algafood.core.storage.StorageProperties;
 import com.algafood.domain.service.FotoStorageService;
 
 @Service
 public class LocalFotoStorageService implements FotoStorageService {
-
-	@Value("${algafood.storage.local.diretorio-fotos}")
-	private Path diretorioFotos;
+	
+	@Autowired
+	StorageProperties storageProperties;
 	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
@@ -53,7 +54,8 @@ public class LocalFotoStorageService implements FotoStorageService {
 	}
 	
 	private Path getArquivoPath(String nomeArquivo) {
-		return diretorioFotos.resolve(Path.of(nomeArquivo));
+		return storageProperties.getLocal().getDiretorioFotos()
+				.resolve(Path.of(nomeArquivo));
 	}
 
 }
