356a94b7d2d11341d05bb67b7d8db45c314fbda1
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2022-01-08 22:26:20 -0300

    Desafio resolvido: funcao para remover foto da amazon-s3 - vídeo 14.24

--- src/main/java/com/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -9,6 +9,7 @@ import com.algafood.core.storage.StorageProperties;
 import com.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
@@ -39,14 +40,23 @@ public class S3FotoStorageService implements FotoStorageService {
 
 			amazonS3.putObject(putObjectRequest);
 		} catch (Exception e) {
-			throw new StorageException("Não foi possível enviar arquivo para Amazon-S3", e);
+			throw new StorageException("Não foi possível enviar arquivo para Amazon-S3.", e);
 		}
 	}
 
 	@Override
 	public void remover(String nomeArquivo) {
-		// TODO Auto-generated method stub
-
+		try {
+			String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
+			
+			var deleteObjectRequest = new DeleteObjectRequest(
+					storageProperties.getS3().getBucket(), 
+					caminhoArquivo);
+			
+			amazonS3.deleteObject(deleteObjectRequest);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+		}
 	}
 
 	@Override
