e1858f2a2f01d851f2b061cb10c597002400b56f
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2022-01-19 23:56:00 -0300

    Ajustando a ordem do envio do e-mail (antes ou depois do commit no bd) - vídeo 15.13

--- src/main/java/com/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -3,6 +3,8 @@ package com.algafood.domain.listener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.algafood.domain.event.PedidoConfirmadoEvent;
 import com.algafood.domain.model.Pedido;
@@ -15,7 +17,10 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	@Autowired
 	private EnvioEmailService envioEmail;
 	
-	@EventListener
+	//@EventListener
+	// se houver erro no envio do e-mail, será feito rollback no banco de dados
+	//@TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+	@TransactionalEventListener
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		Pedido pedido = event.getPedido();
 		
