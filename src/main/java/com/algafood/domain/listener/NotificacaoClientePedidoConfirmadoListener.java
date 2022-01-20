706662c83fd5539f727c6175b91fd729a88a6ecd
Abner Jácomo Pelisser <abner.pelisser@gmail.com>
2022-01-19 23:56:43 -0300

    Limpando imports não utilizados

--- src/main/java/com/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/com/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -1,9 +1,7 @@
 package com.algafood.domain.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.algafood.domain.event.PedidoConfirmadoEvent;
