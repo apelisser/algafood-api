63346e60d84d1aa531bb600caf3ba3dac6b3c297
Abner J Pelisser <abner.pelisser@gmail.com>
2022-02-11 18:53:12 -0300

    Personalizando informacoes de cabecalho na documentacao OAS - vídeo 18.6

--- src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -18,12 +21,24 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 
 	@Bean
 	public Docket apiDocket() {
+		// Acessar documentacao API
+		// http://localhost:8080/swagger-ui/index.html#/
+		
 		return new Docket(DocumentationType.OAS_30)
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.algafood.api"))
 					.paths(PathSelectors.any())
-//					.paths(PathSelectors.ant("/restaurantes/*"))
-					.build();
+					.build()
+				.apiInfo(apiInfo());
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("AlgaFood API :: Abner J Pelisser")
+				.description("Api desenvolvida no curso Especialista Spring Rest - AlgaWorks")
+				.version("1")
+				.contact(new Contact("Abner J Pelisser", "https://github.com/abnerjp", "abner.pelisser@gmail.com"))
+				.build();
 	}
 	
 	@Override
