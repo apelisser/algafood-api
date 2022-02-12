0d1b0b41a8ce3593fdf87b2584a033289101da1c
Abner J Pelisser <abner.pelisser@gmail.com>
2022-02-11 23:25:09 -0300

    Descrevendo códigos de status de respostas de forma global - vídeo 18.12

--- src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algafood/core/openapi/SpringFoxConfig.java
@@ -1,8 +1,13 @@
 package com.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,9 +15,11 @@ import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -40,8 +47,23 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 					.apis(RequestHandlerSelectors.basePackage("com.algafood.api"))
 					.paths(PathSelectors.any())
 					.build()
+				.useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
 				.apiInfo(apiInfo())
 				.tags(tags()[0], tags());
+	}
+	
+	private List<Response> globalGetResponseMessages() {
+		  return Arrays.asList(
+			      new ResponseBuilder()
+			          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+			          .description("Erro interno do Servidor")
+			          .build(),
+			      new ResponseBuilder()
+			          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+			          .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+			          .build()
+			  );
 	}
 	
 	private ApiInfo apiInfo() {
