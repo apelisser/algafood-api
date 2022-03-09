89331e1fe2c75ce7d6b90bdb677e427b5c41e278
Abner J Pelisser <abner.pelisser@gmail.com>
2022-03-08 23:25:03 -0300

    Desafio realizado: descrevendo documentacao de endpoints de associacao de restaurantes com usuários - vídeo 18.33

---
+++ src/main/java/com/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -0,0 +1,56 @@
+package com.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+
+import com.algafood.api.exceptionhandler.Problem;
+import com.algafood.api.model.UsuarioModel;
+
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@Api(tags = "Restaurantes")
+public interface RestauranteUsuarioResponsavelControllerOpenApi {
+
+	@Operation(summary = "Lista os usuários responsáveis associados a restaurante")
+	@ApiResponses({
+		@ApiResponse(responseCode = "404", description = "Restaurante não encontrado",
+				content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+	})
+	public List<UsuarioModel> listar(
+			@Parameter(description = "ID de um restaurante", example = "1", required = true)
+			Long restauranteId);
+
+	@Operation(summary = "Associação de restaurante com usuário responsável")
+	@ApiResponses({
+		@ApiResponse(responseCode = "204", description = "Associação realizada com sucesso"),
+		@ApiResponse(responseCode = "404", description = "Restaurante ou usuário responsável não encotrado", 
+				content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+	})
+	public void associar(
+			@Parameter(description = "ID de um restaurante", example = "1", required = true)
+			Long restauranteId, 
+			
+			@Parameter(description = "ID do usuário", example = "1", required = true)
+			Long usuarioId);
+	
+	@Operation(summary = "Desassociação de restaurante com usuário responsável")
+	@ApiResponses({
+		@ApiResponse(responseCode = "204", description = "Desassociação realizada com sucesso"),
+		@ApiResponse(responseCode = "404", description = "Restaurante ou usuário responsável não encotrado", 
+				content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
+	})	
+	public void desassociar(			
+			@Parameter(description = "ID de um restaurante", example = "1", required = true)
+			Long restauranteId, 
+			
+			@Parameter(description = "ID do usuário", example = "1", required = true)
+			Long usuarioId);
+	
+}
