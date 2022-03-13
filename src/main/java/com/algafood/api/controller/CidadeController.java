66998254b03c5634b9a9a1c968f99b057b9e4fdf
Abner J Pelisser <abner.pelisser@gmail.com>
2022-03-13 18:58:16 -0300

    Adicionando a URI do recurso criado no header da resposta - vídeo 19.2

--- src/main/java/com/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algafood/api/controller/CidadeController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algafood.api.ResourceUriHelper;
 import com.algafood.api.assembler.CidadeInputDisassembler;
 import com.algafood.api.assembler.CidadeModelAssembler;
 import com.algafood.api.model.CidadeModel;
@@ -62,7 +63,13 @@ public class CidadeController implements CidadeControllerOpenApi{
 	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
 		try {
 			Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
-			return cidadeModelAssembler.toModel(cadastroCidade.salvar(cidade));
+			
+			cidade = cadastroCidade.salvar(cidade);
+			CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
+			
+			ResourceUriHelper.addUriInResponseHeader(cidadeModel.getId());
+			
+			return cidadeModel;
 		} catch (EstadoNaoEncontradoException e) {
 			throw new NegocioException(e.getMessage(), e);
 		}
