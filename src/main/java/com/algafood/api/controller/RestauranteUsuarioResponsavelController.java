1e8a85f72320c3f4a0062c9f2587558c2166fadd
Abner J Pelisser <abner.pelisser@gmail.com>
2022-03-23 23:31:33 -0300

    Corrigindo link de colecao de recurso de responsáveis por restaurante - vídeo 19.13

--- src/main/java/com/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -2,6 +2,7 @@ package com.algafood.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,7 +33,11 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
 	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		
-		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis());
+		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis())
+				.removeLinks()
+				.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteUsuarioResponsavelController.class)
+						.listar(restauranteId))
+						.withSelfRel());
 	}
 	
 	@PutMapping("/{usuarioId}")
