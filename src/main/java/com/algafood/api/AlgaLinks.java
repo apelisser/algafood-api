349f9682c71883200dd47fb567cb23f97c9e865a
Abner J Pelisser <abner.pelisser@gmail.com>
2022-04-11 00:16:12 -0300

    Desafio realizado - adicionando links condicionais no recurso de restaurante - vídeo 19.25

--- src/main/java/com/algafood/api/AlgaLinks.java
+++ src/main/java/com/algafood/api/AlgaLinks.java
@@ -122,6 +122,26 @@ public class AlgaLinks {
 		return linkToRestauranteProduto(restauranteId, produtoId, IanaLinkRelations.SELF_VALUE);
 	}
 	
+	public Link linkToRestauranteAtivacao(Long restauranteId, String rel) {
+		return WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteController.class)
+				.ativar(restauranteId)).withRel(rel);
+	}
+
+	public Link linkToRestauranteInativacao(Long restauranteId, String rel) {
+		return WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteController.class)
+				.inativar(restauranteId)).withRel(rel);
+	}
+	
+	public Link linkToRestauranteAbertura(Long restauranteId, String rel) {
+		return WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteController.class)
+				.abrir(restauranteId)).withRel(rel);
+	}
+
+	public Link linkToRestauranteFechamento(Long restauranteId, String rel) {
+		return WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteController.class)
+				.fechar(restauranteId)).withRel(rel);
+	}
+	
 	public Link linkToEstado(Long estadoId, String rel) {
 		return WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(EstadoController.class)
 				.buscar(estadoId)).withRel(rel);		
