640f58673eb354ca3f39eb28c5685ebe94da1a8b
Abner J Pelisser <abner.pelisser@gmail.com>
2022-08-06 01:13:51 -0300

    corrigindo a documentacao na documentacao - vídeo 19.41

--- src/main/java/com/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,10 +1,31 @@
 package com.algafood.api.openapi.model;
 
-import com.algafood.api.model.CozinhaModel;
+
+import java.util.List;
+
+import org.springframework.hateoas.Links;
 
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import com.algafood.api.model.CozinhaModel;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel>{
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
 	
+	private CozinhasEmbeddedModelOpenApi _embedded;
+	private Links _links;
+	private PageModelOpenApi page;
+	
+	@ApiModel("CozinhasEmbeddedModel")
+	@Data
+	private class CozinhasEmbeddedModelOpenApi {
+		
+		private List<CozinhaModel> cozinha;
+		
+	}
 }
