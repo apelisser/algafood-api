f10890908e0213e77016847aeb0f51a0c176cf83
Abner J Pelisser <abner.pelisser@gmail.com>
2022-08-14 11:01:51 -0300

    implementando o versionamento da api por media type - video 20.11

--- src/main/java/com/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,20 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algafood.api.v2.model.input.CidadeInputV2;
+import com.algafood.domain.model.Cidade;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		modelMapper.createTypeMap(CidadeInputV2.class, Cidade.class)
+			.addMappings(mapper -> mapper.skip(Cidade::setId));
+		
+		return modelMapper;
 	}
 	
 }
