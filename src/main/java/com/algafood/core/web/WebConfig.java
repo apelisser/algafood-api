0d984fe64cda228ade18e9afbfe18a9f6a354773
Abner J Pelisser <abner.pelisser@gmail.com>
2022-08-15 12:25:27 -0300

    definindo a versao padrao da api quando o media type nao e informado - video 20.12

--- src/main/java/com/algafood/core/web/WebConfig.java
+++ src/main/java/com/algafood/core/web/WebConfig.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
 		.allowedOrigins("*")
 		.allowedMethods("*");
 //		.maxAge(30);
+	}
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		// definir media type padrao, caso nao seja informado
+		// neste caso: media type para padronizar a versao 2 da api
+		configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATION_JSON);
 	}
 	
 	@Bean
