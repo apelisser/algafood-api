83c87dda174bc465bd32c967b182e673b0215bd8
Abner J Pelisser <abner.pelisser@gmail.com>
2022-10-13 01:25:08 -0300

    juntando o resource server com o authorization server no mesmo projeto - video 23.41

--- src/main/java/com/algafood/core/web/WebConfig.java
+++ src/main/java/com/algafood/core/web/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,13 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Autowired 
 	private ApiRetirementHandler apiDeprecationHandler;
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-		.allowedOrigins("*")
-		.allowedMethods("*");
-//		.maxAge(30);
-	}
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
