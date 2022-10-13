133ad733a1a8290139647e2420abdb54df8513b1
Abner J Pelisser <abner.pelisser@gmail.com>
2022-10-13 12:56:30 -0300

    ajustando a documentacao da api para suporte a oauth2 - video 23.42

--- src/main/java/com/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algafood/core/springfox/SpringFoxConfig.java
@@ -56,6 +56,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -63,10 +64,16 @@ import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
 import springfox.documentation.service.Response;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -88,14 +95,14 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 		return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
 	}
 	
-//	@Bean
+	@Bean
 	public Docket apiDocketV1() {
 		// Acessar documentacao API
 		// http://localhost:8080/swagger-ui/index.html#/
 		
 		TypeResolver typeResolver = new TypeResolver();
 		
-		return new Docket(DocumentationType.OAS_30)
+		return new Docket(DocumentationType.SWAGGER_2)
 				.groupName("V1")
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.algafood.api"))
@@ -142,11 +149,15 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 				.alternateTypeRules(AlternateTypeRules.newRule(
 						typeResolver.resolve(CollectionModel.class, CidadeModel.class), 
 						CidadesModelOpenApi.class))
+				
+				.securitySchemes(Arrays.asList(securityScheme()))
+				.securityContexts(Arrays.asList(securityContext()))
+				
 				.apiInfo(apiInfoV1())
 				.tags(tagsV1()[0], tagsV1());
 	}
 	
-	@Bean
+//	@Bean
 	public Docket apiDocketV2() {
 		// Acessar documentacao API
 		// http://localhost:8080/swagger-ui/index.html#/
@@ -285,5 +296,34 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 			new Tag("Cidades", "Gerencia as cidades"),
 			new Tag("Cozinhas", "Gerencia as cozinhas")
 		};
+	}
+	
+	private SecurityScheme securityScheme() {
+		return new OAuthBuilder()
+				.name("AlgaFood")
+				.grantTypes(grantTypes())
+				.scopes(scopes())
+				.build();
+	}
+
+	private SecurityContext securityContext() {
+		var securityReference = SecurityReference.builder()
+				.reference("AlgaFood")
+				.scopes(scopes().toArray(new AuthorizationScope[0]))
+				.build();
+		
+		return SecurityContext.builder()
+				.securityReferences(Arrays.asList(securityReference))
+				.forPaths(PathSelectors.any())
+				.build();
+	}
+	
+	private List<GrantType> grantTypes() {
+		return Arrays.asList(new ResourceOwnerPasswordCredentialsGrant("/oauth/token"));
+	}
+	
+	private List<AuthorizationScope> scopes() {
+		return Arrays.asList(new AuthorizationScope("READ", "Acesso de leitura"),
+				new AuthorizationScope("WRITE", "Acesso de escrita"));
 	}
 }
