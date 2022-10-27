98078ff30ae0f01fb88cd6489f7ce95c44c1d9ab
Abner J Pelisser <abner.pelisser@gmail.com>
2022-10-26 22:13:24 -0300

    organizar arquivo properties e dependencia de javamailsender nao satisfeita: melhorando o uso da heranca - videos 25.2 e 25.3

--- src/main/java/com/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -6,13 +6,9 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.algafood.core.email.EmailProperties;
 import com.algafood.domain.service.EnvioEmailService;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 
 public class SmtpEnvioEmailService implements EnvioEmailService {
 
@@ -21,9 +17,9 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	
 	@Autowired
 	private EmailProperties emailProperties;
-	
+
 	@Autowired
-	private Configuration freeMarkerConfig;
+	private ProcessadorEmailTemplate processadorEmailTemplate;
 	
 	@Override
 	public void enviar(Mensagem mensagem) {
@@ -33,10 +29,10 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 		} catch (Exception e) {
 			throw new EmailException("Não foi possível enviar e-mail", e);
 		}
-	}
+	};
 	
 	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-		String corpo = processarTemplate(mensagem);
+		String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 		
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		
@@ -49,15 +45,4 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 		return mimeMessage;
 	}
 	
-	protected String processarTemplate(Mensagem mensagem) {
-		try {
-			Template template = freeMarkerConfig.getTemplate(mensagem.getCorpo());
-			
-			return FreeMarkerTemplateUtils.processTemplateIntoString(
-					template, mensagem.getVariaveis());
-		} catch (Exception e) {
-			throw new EmailException("Não foi possível montar o template do e-mail", e);
-		} 
-	}
-
 }
