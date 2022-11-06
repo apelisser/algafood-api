1b67470e4900bb85717d2064250351ac84256dc8
Abner J Pelisser <abner.pelisser@gmail.com>
2022-11-06 17:02:50 -0300

    descrevendo as operacoes de enpoints na documentacao - video 26.7

--- src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -5,6 +5,7 @@ import org.springframework.hateoas.CollectionModel;
 import com.algafood.api.v1.model.CidadeModel;
 import com.algafood.api.v1.model.input.CidadeInput;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -12,13 +13,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Cidades")
 public interface CidadeControllerOpenApi {
 
+	@Operation(summary = "Lista as cidades")
 	public CollectionModel<CidadeModel> listar();
 	
+	@Operation(summary = "Busca uma cidade por ID")
 	public CidadeModel buscar(Long cidadeId);
 	
+	@Operation(summary = "Cadastra uma cidade", 
+			description = "Cadastro de uma cidade, necessita de um estado e um nome válido")
 	public CidadeModel adicionar(CidadeInput cidadeInput);
 	
+	@Operation(summary = "Atualiza uma cidade por ID")
 	public CidadeModel atualizar(Long cidadeId, CidadeInput cidadeInput);
 
+	@Operation(summary = "Exclui uma cidade pelo ID")
 	public void remover(Long cidadeId);
 }
