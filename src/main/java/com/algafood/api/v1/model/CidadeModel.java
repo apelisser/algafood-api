74d926a5436171f796635d8fc915dfba5c67d3cd
Abner J Pelisser <abner.pelisser@gmail.com>
2022-11-06 21:27:51 -0300

    descrevendo modelos de representacoes e suas propriedades - video 26.9

--- src/main/java/com/algafood/api/v1/model/CidadeModel.java
+++ src/main/java/com/algafood/api/v1/model/CidadeModel.java
@@ -3,6 +3,7 @@ package com.algafood.api.v1.model;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +13,12 @@ import lombok.Setter;
 @Setter
 public class CidadeModel extends RepresentationModel<CidadeModel> {
 
+	@Schema(example = "1")
 	private Long id;
+	
+	@Schema(example = "Presidente Prudente")
 	private String nome;
+	
 	private EstadoModel estado;
 	
 }
