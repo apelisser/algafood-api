655e7a252c1831d9ed3c0899d0d13d90030e87ef
Abner J Pelisser <abner.pelisser@gmail.com>
2022-11-07 01:32:57 -0300

    descrevendo o modelo de representacao de problema - video 26.14

--- src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -24,7 +24,8 @@ public interface CidadeControllerOpenApi {
 	@Operation(summary = "Busca uma cidade por ID", responses = {
 			@ApiResponse(responseCode = "200"),
 			@ApiResponse(responseCode = "400", description = "ID da cidade inválido", 
-				content = @Content(schema = @Schema))
+				content = @Content(schema = @Schema(ref = "Problema"))
+			)
 	})
 	public CidadeModel buscar(
 			@Parameter(description = "ID de uma cidade", example = "1", required = true)
--- src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
@@ -1,21 +1,28 @@
 package com.algafood.core.springdoc;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algafood.api.exceptionhandler.Problem;
+
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
@@ -48,7 +55,10 @@ public class SpringDocConfig {
 						).externalDocs(new ExternalDocumentation()
 								.description("Abner Jácomo Pelisser")
 								.url("https://www.linkedin.com/in/abner-pelisser/")
-						).tags(loadTags());
+						).tags(loadTags())
+						.components(new Components().schemas(
+								gerarSchemas()
+						));
 	}
 	
 	@Bean
@@ -96,5 +106,15 @@ public class SpringDocConfig {
 			);
 	}
 	
+	private Map<String, Schema> gerarSchemas() {
+		final Map<String, Schema> schemaMap = new HashMap<>();
+		
+		Map<String, Schema> problemSchema = ModelConverters.getInstance().read(Problem.class);
+		Map<String, Schema> problemObjectSchema = ModelConverters.getInstance().read(Problem.Object.class);
+		
+		schemaMap.putAll(problemSchema);
+		schemaMap.putAll(problemObjectSchema);
+		return schemaMap;
+	}
 	
 }
