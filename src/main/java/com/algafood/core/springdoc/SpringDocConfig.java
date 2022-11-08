0dd829a0af2555e442daf954da4f50844d9fda7b
Abner J Pelisser <abner.pelisser@gmail.com>
2022-11-07 23:22:05 -0300

    desafio realizado - descrevendo documentacao de endpoints de grupos - video 26.16

--- src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
@@ -107,7 +107,8 @@ public class SpringDocConfig {
 	
 	private List<Tag> loadTags() {
 		return Arrays.asList(
-				new Tag().name("Cidades").description("Gerencia as cidades")
+				new Tag().name("Cidades").description("Gerencia as cidades"),
+				new Tag().name("Grupos").description("Gerencia os grupos")
 			);
 	}
 	
