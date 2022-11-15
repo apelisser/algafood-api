3bbd2d8d1ac549e91ffd0acca688e2e119d2ef73
Abner J Pelisser <abner.pelisser@gmail.com>
2022-11-15 02:09:35 -0300

    desafio realizado - descrevendo documentacao de endpoints de estados - video 26.25

--- src/main/java/com/algafood/api/v1/openapi/controller/EstadoControllerOpenApi.java
+++ src/main/java/com/algafood/api/v1/openapi/controller/EstadoControllerOpenApi.java
@@ -5,19 +5,68 @@ import org.springframework.hateoas.CollectionModel;
 import com.algafood.api.v1.model.EstadoModel;
 import com.algafood.api.v1.model.input.EstadoInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Estados")
 public interface EstadoControllerOpenApi {
 
+	@Operation(summary = "Lista os estados")
 	public CollectionModel<EstadoModel> listar();
 
-	public EstadoModel buscar(Long estadoId);
+	@Operation(summary = "Busca um estado por ID", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID do estado inválido", 
+				content = @Content(schema = @Schema(ref = "Problema"))
+			),
+			@ApiResponse(responseCode = "404", description = "Estado não encontrado", 
+				content = @Content(schema = @Schema(ref = "Problema"))
+			)
+	})
+	public EstadoModel buscar(
+			@Parameter(description = "ID de um estado", example = "1", required = true)
+			Long estadoId);
 
-	public EstadoModel adicionar(EstadoInput estadoInput);
+	@Operation(summary = "Cadastra um estado", responses = {
+			@ApiResponse(responseCode = "201", description = "Estado cadastrado"),
+	})
+	public EstadoModel adicionar(
+			@RequestBody(description = "Representação de um novo estado", required = true)			
+			EstadoInput estadoInput);
 
-	public EstadoModel atualizar(Long estadoId, EstadoInput estadoInput);
+	@Operation(summary = "Atualiza um estado por ID", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID do estado inválido", 
+				content = @Content(schema = @Schema(ref = "Problema"))
+			),
+			@ApiResponse(responseCode = "404", description = "Estado não encontrado", 
+				content = @Content(schema = @Schema(ref = "Problema"))
+			)
+	})
+	public EstadoModel atualizar(
+			@Parameter(description = "ID de um estado", example = "1", required = true)
+			Long estadoId, 
+			@RequestBody(description = "Representação de um estado com dados atualizados", required = true)			
+			EstadoInput estadoInput);
 
-	public void remover(Long estadoId);
+	@Operation(summary = "Exclui um estado pelo ID", responses = {
+			@ApiResponse(responseCode = "204"),
+			@ApiResponse(responseCode = "400", description = "ID do estado inválido", 
+				content = @Content(schema = @Schema(ref = "Problema"))
+			),
+			@ApiResponse(responseCode = "404", description = "Estado não encontrado", 
+				content = @Content(schema = @Schema(ref = "Problema"))
+			)
+	})
+	public void remover(
+			@Parameter(description = "ID de um estado", example = "1", required = true)
+			Long estadoId);
 
 }
--- src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algafood/core/springdoc/SpringDocConfig.java
@@ -111,6 +111,7 @@ public class SpringDocConfig {
 				new Tag().name("Grupos").description("Gerencia os grupos"),
 				new Tag().name("Cozinhas").description("Gerencia as cozinhas"),
 				new Tag().name("Pedidos").description("Gerencia os pedidos"),
+				new Tag().name("Estados").description("Gerencia os estados"),
 				new Tag().name("Restaurantes").description("Gerencia os restaurantes"),
 				new Tag().name("Formas de pagamento").description("Gerencia as formas de pagamento")
 			);
