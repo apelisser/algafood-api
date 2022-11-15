ac8c51b6d5da2c46d0e33099b08e50061aaac7de
Abner J Pelisser <abner.pelisser@gmail.com>
2022-11-15 00:08:25 -0300

    descrevendo media type da resposta nos endpoints - video 26.22

--- src/main/java/com/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ src/main/java/com/algafood/api/v1/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -9,6 +9,10 @@ import org.springframework.web.multipart.MultipartFile;
 import com.algafood.api.v1.model.FotoProdutoModel;
 import com.algafood.api.v1.model.input.FotoProdutoInput;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name = "security_auth")
@@ -17,8 +21,16 @@ public interface RestauranteProdutoFotoControllerOpenApi {
 	public FotoProdutoModel atualizarFoto(Long restauranteId, Long produtoId, FotoProdutoInput fotoProdutoInput,
 			MultipartFile arquivo) throws IOException;
 
+	@Operation(summary = "Buscar a foto do produto de um restaurante", responses = {
+			@ApiResponse(responseCode = "200", content = {
+					@Content(mediaType = "aplication/json", schema = @Schema(implementation = FotoProdutoModel.class)),
+					@Content(mediaType = "image/jpeg", schema = @Schema(type = "string", format = "binary")),
+					@Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary"))
+			})
+	})
 	public FotoProdutoModel buscar(Long restauranteId, Long produtoId);
 
+	@Operation(hidden = true)
 	public ResponseEntity<?> servir(Long restauranteId, Long produtoId, String acceptHeader)
 			throws HttpMediaTypeNotAcceptableException;
 
