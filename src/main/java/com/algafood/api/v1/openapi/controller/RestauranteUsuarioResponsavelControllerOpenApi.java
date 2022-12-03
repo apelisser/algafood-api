e48f097d9c800233c3898cb73f12090fa023f650
Abner J Pelisser <abner.pelisser@gmail.com>
2022-12-02 23:06:28 -0300

    desafio reslizado - descrevendo documentacao de endpoints de associacao de restaurantes com usuarios - video 26.28

--- src/main/java/com/algafood/api/v1/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
+++ src/main/java/com/algafood/api/v1/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -5,15 +5,50 @@ import org.springframework.http.ResponseEntity;
 
 import com.algafood.api.v1.model.UsuarioModel;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Restaurantes")
 public interface RestauranteUsuarioResponsavelControllerOpenApi {
 
-	public CollectionModel<UsuarioModel> listar(Long restauranteId);
+	@Operation(summary = "Lista os usuários responsáveis associados a restaurante", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "404", description = "Restaurante não encontrado", content = {
+					@Content(schema = @Schema(ref = "Problema"))
+			})
+	})
+	public CollectionModel<UsuarioModel> listar(
+			@Parameter(description = "ID do restaurante", example = "1", required = true)
+			Long restauranteId);
 
-	public ResponseEntity<Void> associar(Long restauranteId, Long usuarioId);
+	@Operation(summary = "Asociação de restaurante com usuário responsável", responses = {
+			@ApiResponse(responseCode = "204", description = "Associação realizada com sucesso"),
+			@ApiResponse(responseCode = "404", description = "Restaurante ou usuário não encontrado", content = {
+					@Content(schema = @Schema(ref = "Problema"))
+			})
+	})
+	public ResponseEntity<Void> associar(
+			@Parameter(description = "ID do restaurante", example = "1", required = true)
+			Long restauranteId, 
+			@Parameter(description = "ID do usuário", example = "1", required = true)
+			Long usuarioId);
 
-	public ResponseEntity<Void> desassociar(Long restauranteId, Long usuarioId);
+	@Operation(summary = "Desassociação de restaurante com usuário responsável", responses = {
+			@ApiResponse(responseCode = "204", description = "Desassociação realizada com sucesso"),
+			@ApiResponse(responseCode = "404", description = "Restaurante ou usuário não encontrado", content = {
+					@Content(schema = @Schema(ref = "Problema"))
+			})
+	})
+	public ResponseEntity<Void> desassociar(
+			@Parameter(description = "ID do restaurante", example = "1", required = true)
+			Long restauranteId, 
+			@Parameter(description = "ID do usuário", example = "1", required = true)
+			Long usuarioId);
 
 }
