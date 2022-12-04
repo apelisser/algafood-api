a8551fba654910c739e43ba2774fc5f127be1832
Abner J Pelisser <abner.pelisser@gmail.com>
2022-12-04 14:54:06 -0300

    desafio realizado - descrevendo documentacao de endpoint de estatisticas - video 26.34

--- src/main/java/com/algafood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
+++ src/main/java/com/algafood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
@@ -8,15 +8,48 @@ import com.algafood.api.v1.controller.EstatisticasController.EstatisticasModel;
 import com.algafood.domain.filter.VendaDiariaFilter;
 import com.algafood.domain.model.dto.VendaDiaria;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Estatísticas")
 public interface EstatisticasControllerOpenApi {
 
-	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
+	@Operation(
+			summary = "Consulta estatísticas de vendas diárias",
+			parameters = {
+					@Parameter(in = ParameterIn.QUERY, name = "restauranteId", description = "ID do restaurante", 
+							example = "1", schema = @Schema(type = "integer")),
+					@Parameter(in = ParameterIn.QUERY, name = "dataCriacaoInicio", description = "Data/hora inicial da criação do pedido", 
+							example = "2022-12-01T00:00:00Z", schema = @Schema(type = "string", format = "date-time")),
+					@Parameter(in = ParameterIn.QUERY, name = "dataCriacaoFim", description = "Data/hora final da criação do pedido", 
+							example = "2022-12-04T23:59:59Z", schema = @Schema(type = "string", format = "date-time"))
+			},
+			responses = {
+					@ApiResponse(responseCode = "200", content = {
+							@Content(mediaType = "aplication/json", array = @ArraySchema(schema = @Schema(implementation = VendaDiaria.class))),
+							@Content(mediaType = "application/pdf", schema = @Schema(type = "string", format = "binary"))
+					})
+			}
+	)
+	public List<VendaDiaria> consultarVendasDiarias(
+			@Parameter(hidden = true)
+			VendaDiariaFilter filtro, 
+			@Parameter(description = "Deslocamento de horário a ser considerado na consulta em relação ao UTC", 
+				schema = @Schema(type = "string", defaultValue = "+00:00"))
+			String timeOffset);
 
+	@Operation(hidden = true)
 	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro, String timeOffset);
 
+	@Operation(hidden = true)
 	public EstatisticasModel estatisticas();
 	
 }
