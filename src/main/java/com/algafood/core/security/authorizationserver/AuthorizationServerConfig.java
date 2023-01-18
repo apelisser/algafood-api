53c4e6fa31c09590a89e99030ca9d683d3c10e62
Abner J Pelisser <abner.pelisser@gmail.com>
2023-01-18 17:20:09 -0300

    configurando a geracao de access token jwt no authorization server - video 27.9

--- src/main/java/com/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -1,5 +1,8 @@
 package com.algafood.core.security.authorizationserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.time.Duration;
 import java.util.Arrays;
 
@@ -7,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
@@ -22,6 +26,12 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 
 @Configuration
@@ -52,7 +62,7 @@ public class AuthorizationServerConfig {
 				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
 				.scope("READ")
 				.tokenSettings(TokenSettings.builder()
-						.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+						.accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
 						.accessTokenTimeToLive(Duration.ofMinutes(30))
 						.build())
 				.build();
@@ -64,5 +74,19 @@ public class AuthorizationServerConfig {
 	public OAuth2AuthorizationService oAuth2AuthorizationService(JdbcOperations jdbcOperations,
 			RegisteredClientRepository registeredClientRepository) {
 		return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
+	}
+	
+	@Bean
+	public JWKSource<SecurityContext> jwkSource(JwtKeyStoreProperties properties) throws Exception {
+		char[] keyStorePass = properties.getPassword().toCharArray();		
+		String keypairAlias = properties.getKeypairAlias();
+		
+		Resource jksLocation = properties.getJksLocation();
+		InputStream inputStream = jksLocation.getInputStream();
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+		keyStore.load(inputStream, keyStorePass);
+		
+		RSAKey rsaKey = RSAKey.load(keyStore, keypairAlias, keyStorePass);
+		return new ImmutableJWKSet<>(new JWKSet(rsaKey));
 	}
 }
