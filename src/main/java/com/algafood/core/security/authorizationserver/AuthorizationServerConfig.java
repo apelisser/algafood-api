f7b464b0ca27317df68f654fa8f6f326701550bd
Abner J Pelisser <abner.pelisser@gmail.com>
2023-01-20 13:22:05 -0300

    revogando consentimentos e autorizacoes dos clientes - video 27.21

--- src/main/java/com/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -123,8 +123,9 @@ public class AuthorizationServerConfig {
 	}
 	
 	@Bean
-	public OAuth2AuthorizationQueryService oAuth2AuthorizationQueryService(JdbcOperations jdbcOperations) {
-		return new JdbcOAuth2AuthorizationQueryService(jdbcOperations);
+	public OAuth2AuthorizationQueryService oAuth2AuthorizationQueryService(JdbcOperations jdbcOperations, 
+			RegisteredClientRepository repository) {
+		return new JdbcOAuth2AuthorizationQueryService(jdbcOperations, repository);
 	}
 
 }
